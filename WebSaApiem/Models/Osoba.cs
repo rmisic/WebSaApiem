9405016d7f4906ba86f5eff2181ccd34bb53973e
gitcontrolsetings <misicratko.sicmi@gmail.com>
2023-03-11 09:21:54 +0100

    Add project files.

---
+++ WebSaApiem/Models/Osoba.cs
@@ -0,0 +1,14 @@
+﻿namespace WebSaApiem.Models
+{
+    public class Osoba
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string BrojTelefona { get; set;}
+        public string BrojMobitela { get; set;}
+        public string Adresa { get; set;}
+        public string Grad { get; set; }
+        public string Drzava { get; set;}
+    }
+}
